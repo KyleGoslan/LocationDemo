e80dc5429805ff7aece06d165b45f255cc7e65d9
Kyle Goslan <KyleGoslan@users.noreply.github.com>
2018-02-20 13:39:52 +0000

    Added pods

--- LocationDemo/AppDelegate.swift
+++ LocationDemo/AppDelegate.swift
@@ -6,13 +6,17 @@
 //  Copyright © 2017 Kyle Goslan. All rights reserved.
 //
 
-import UIKit
+import UIKit 
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
+  override init() {
+    super.init()
+    
+  }
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '11.0'
+
+target 'LocationDemo' do
+
+  use_frameworks!
+
+  pod 'Firebase/Core'
+  pod 'FirebaseInstanceID', '2.0.5'
+  pod 'Firebase/Database'
+  pod 'Firebase/Auth'
+  pod 'GeoFire', :git => 'https://github.com/firebase/geofire-objc.git'
+
+end
